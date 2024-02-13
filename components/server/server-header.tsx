a110a32950d529b4ae9d4dec2911a93ee330daf3
imminent-byte <imminent11225@gmail.com>
2024-02-13 19:02:33 +0300

    created the members modal, implementing functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -47,7 +47,7 @@ const ServerHeader = ({
             )}
             {isAdmin && (
                 <DropdownMenuItem onClick={() => onOpen("editServer", { server })} className="px-3 py-2 text-sm cursor-pointer">
-                    Server Settings
+                    Domain Settings
                     <Settings className="h-4 w-4 ml-auto"/>
                 </DropdownMenuItem>
             )}
