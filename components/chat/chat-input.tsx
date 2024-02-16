56a52f75fbb9fc1272657a7106d6e51a00589050
imminent-byte <imminent11225@gmail.com>
2024-02-16 12:22:24 +0300

    completed input, next create api

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,6 +1,21 @@
 "use client"
 
+import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem
+} from "../ui/form"
+
+import { Input } from '../ui/input';
+import { Paperclip, Smile } from 'lucide-react';
+
+import axios from 'axios';
+import queryString from 'query-string';
 
 interface ChatInputProps {
     apiUrl: string;
@@ -9,7 +24,7 @@ interface ChatInputProps {
     type: "conversation" | "channel";
 }
 
-const formShema = z.object({
+const formSchema = z.object({
     content: z.string().min(1),
 })
 
@@ -19,7 +34,60 @@ export const ChatInput = ({
     name,
     type
 }: ChatInputProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            content: "",
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (value: z.infer<typeof formSchema>) => {
+        try {
+            const url = queryString.stringifyUrl({
+                url: apiUrl,
+                query,
+            });
+
+            await axios.post(url, value);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div>input</div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                    control={form.control}
+                    name='content'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <div className='relative p-4 pb-6'>
+                                    <button
+                                        type='button'
+                                        onClick={() => {}}
+                                        className='absolute top-7 left-8 h-[24px] w-[24px] bg-lmeffects dark:bg-dmeffects hover:bg-lmlinks dark:hover:bg-dmlinks transition rounded-full p-1 flex items-center justify-center'
+                                    >
+                                        <Paperclip className='text-lmtext dark:text-dmtext dark:hover:text-dmbg'/>
+                                    </button>
+                                    <Input
+                                        disabled={isLoading}
+                                        placeholder={`Message ${type === "conversation" ? name : name}`}
+                                        className='px-14 py-6 bg-lmsbg dark:bg-dmeffects/5 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-lmtext dark:text-dmtext'
+                                        {...field}
+                                    />
+                                    <div className='absolute top-7 right-8'>
+                                        <Smile className='dark:text-dmeffects dark:hover:text-dmlinks'/>
+                                    </div>
+                                </div>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
     )
 }
