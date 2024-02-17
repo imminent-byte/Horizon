cb72355466d999ddc235e4251e518467fcc3144a
imminent-byte <imminent11225@gmail.com>
2024-02-17 07:34:27 +0300

    clicking on member redirects to dms

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -8,6 +8,8 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
+import { useRouter, useParams } from "next/navigation";
+
 import * as z from"zod";
 import axios from "axios";
 import queryString from "query-string";
@@ -62,6 +64,17 @@ export const ChatItem = ({
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false);
     const { onOpen } = useModal();
+
+    const params = useParams();
+    const router = useRouter();
+
+    const onMemberClick = () => {
+        if(member.id == currentMember.id) {
+            return;
+        }
+
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+    }
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -119,13 +132,13 @@ export const ChatItem = ({
     return (
         <div className="relative group flex items-center hover:bg-dmlinks/55 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-                <div className="cursor-pointer hover:drop-shadow-md transition">
+                <div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
                     <UserAvatar src={member.profile.imageUrl}/>
                 </div>
                 <div className="flex flex-col w-full">
                     <div className="flex items-center gap-x-2">
                         <div className="flex items-center">
-                            <p className="font-semibold text-sm hover:underline cursor-pointer">
+                            <p onClick={onMemberClick} className="font-semibold text-sm hover:underline cursor-pointer">
                                 {member.profile.name}
                             </p>
                             <ActionTooltip label={member.role}>
