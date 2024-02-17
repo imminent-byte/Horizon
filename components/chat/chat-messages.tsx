391f68882bc9f02c8af652e6c51d3f74e9118285
imminent-byte <imminent11225@gmail.com>
2024-02-17 08:09:27 +0300

    chat messages are loaded when clicked on load messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { Fragment, useRef, ElementRef } from "react";
+
 import { Member, Message, Profile } from "@prisma/client";
 import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
-import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
 import { format } from "date-fns"
 import { useChatSocket } from "@/hooks/use-chat-socket";
@@ -44,6 +45,9 @@ const ChatMessages = ({
     const addKey = `chat:${chatId}:messages`
     const updateKey = `chat:${chatId}:messages:update`
 
+    const chatRef = useRef<ElementRef<"div">>(null)
+    const bottomRef = useRef<ElementRef<"div">>(null)
+    
     const { 
         data,
         fetchNextPage,
@@ -71,19 +75,30 @@ const ChatMessages = ({
             <div className="flex flex-col flex-1 justify-center items-center">
                 <ServerCrash className="h-7 w-7 text-dmlinks my-4"/>
                 <p className="text-xs text-lmeffects dark:text-dmeffects">
-                    Something Went Wrong
+                    Something Went Wrong!
                 </p>
             </div>
         )
     }
 
   return (
-    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-        <div className="flex-1">
-            <ChatWelcome
+    <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
+        {!hasNextPage && <div className="flex-1" />}
+            {!hasNextPage && <ChatWelcome
                 type={type}
                 name={name}
-            />
+            />}
+            {hasNextPage && (
+                <div className="flex justify-center">
+                    {isFetchingNextPage ? (
+                        <Loader2 className="h-6 w-6 text-dmeffects animate-spin my-4"/>
+                    ): (
+                        <button onClick={() => fetchNextPage()} className="text-lmeffects hover:text-lmlinks dark:text-dmlinks/30 dark:hover:text-dmeffects text-xs my-4 transition">
+                            Load Older Messages
+                        </button>
+                    )}
+                </div>
+            )}
             <div className="flex flex-col-reverse mt-auto">
                 {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
@@ -105,8 +120,8 @@ const ChatMessages = ({
                     </Fragment>
                 ))}
             </div>
+            <div ref={bottomRef}/>
         </div>
-    </div>
   )
 }
 
